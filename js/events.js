3a71f242bd7bd678160074476bc64c67a227abfa
pouria <pouriaff7171@gmail.com>
2025-10-30 16:57:26 +0330

    changes

--- js/events.js
+++
@@ -1,3 +0,0 @@
-const bus = new EventTarget();
-export const emit = (name, detail) => bus.dispatchEvent(new CustomEvent(name, { detail }));
-export const on = (name, handler) => bus.addEventListener(name, handler);
